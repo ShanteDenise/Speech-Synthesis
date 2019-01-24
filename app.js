cc8f4923ca0834ab391a488857492ac472a75d5a
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-24 14:25:30 -0500

    Added media queries

--- app.js
+++ app.js
@@ -11,27 +11,28 @@ const stopButton = document.querySelector('#stop')
 msg.text = document.querySelector('[name="text"]').value
 
 function populateVoices() {
+    //Get voices from getVoices function built in 
     voices = this.getVoices();
+    //Add voices to html
     voicesDropdown.innerHTML = voices
-      .filter(voice => voice.lang.includes('en'))
-      .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
-      .join('');
+      .filter(voice => voice.lang.includes('en')) //Filter array of voices by language
+      .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)//map through array of voices to only select name and voice language
+      .join(''); 
   }
 
   function setVoice(){
-     msg.voice = voices.find(voice => voice.name === this.value);
+     msg.voice = voices.find(voice => voice.name === this.value); //set selected voice to the name of that voice
   }
 
-  function toggle(startOver = true){
+  function toggle(startOver = true){ //on toggle of the sliders call method cancel to stop the sound
       speechSynthesis.cancel()
-      if(startOver){
-    //   speechSynthesis.cancel();
+      if(startOver){ //if not speak
       speechSynthesis.speak(msg)
       }
   }
 
   function setOption(){
-      msg[this.name] = this.value
+      msg[this.name] = this.value //set name to call method toggle to get text to start talking
       toggle()
   }
 
