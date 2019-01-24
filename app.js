8067300e44fb376188333ab0d853e77df8755839
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-24 10:56:03 -0500

    Added function for populating voices to options

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const msg = new SpeechSynthesisUtterance()
 
 // Declared variables 
 let voices = []
-const voicesDropdown = $('[name="voice"]')
-const options = $('[type="range"], [name="text"]')
-const speakButton = $('#speak')
-const stopButton = $('#stop')
+const voicesDropdown = document.querySelector('[name="voice"]')
+const options = document.querySelectorAll('[type="range"], [name="text"]')
+const speakButton = document.querySelector('#speak')
+const stopButton = document.querySelector('#stop')
 
-msg.text = $('[name="text"]').value
+msg.text = document.querySelector('[name="text"]').value
+
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name}(${voice.lang}</option>`)
+    .join('')
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+
 
