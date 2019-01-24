001feeca8a1c57e584fbfbb2bd9cf382841d5083
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-24 13:54:27 -0500

    Added new font

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ msg.text = document.querySelector('[name="text"]').value
 function populateVoices() {
     voices = this.getVoices();
     voicesDropdown.innerHTML = voices
-    //   .filter(voice => voice.lang.includes('en'))
+      .filter(voice => voice.lang.includes('en'))
       .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
       .join('');
   }
@@ -22,13 +22,23 @@ function populateVoices() {
      msg.voice = voices.find(voice => voice.name === this.value);
   }
 
-  function toggle(){
-      speechSynthesis.cancel();
+  function toggle(startOver = true){
+      speechSynthesis.cancel()
+      if(startOver){
+    //   speechSynthesis.cancel();
       speechSynthesis.speak(msg)
+      }
+  }
 
+  function setOption(){
+      msg[this.name] = this.value
+      toggle()
   }
 
   speechSynthesis.addEventListener('voiceschanged', populateVoices);
   voicesDropdown.addEventListener('change', setVoice);
+  options.forEach(option => option.addEventListener('change', setOption))
+  speakButton.addEventListener('click', toggle)
+  stopButton.addEventListener('click', () => toggle(false));
 
 
